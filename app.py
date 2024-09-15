15d5eee67cefd750166f83476abdfbeab302f6fd
FriendlyFire227 <suskindenis74@gmail.com>
2024-09-15 12:29:26 +0300

    Исправил кнопку

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ tab1, tab2, tab3 = st.tabs(["Дорогой ребенок","Менее доро
 with tab1:
     st.title("Дорогой")
     st.image("fedb30b299a3e2873af377e2cf46ad5a.jpg")
+    if st.button("Купить1"):
+        st.success("Успешно куплено, разрешено перепродать")
 with tab2:
     st.title("Средний")
     st.image("0b8d922ba7be7c65717e084c7c544090.jpg")
+    if st.button("Купить2"):
+        st.success("Успешно куплено, разрешено перепродать")
 with tab3:
     st.title("Дешовый")
     st.image("scale_1200.jpg")
-if st.button("Купить"):
-    st.success("Успешно куплено, разрешено перепродать")
+    if st.button("Купить3"):
+        st.success("Успешно куплено, разрешено перепродать")
